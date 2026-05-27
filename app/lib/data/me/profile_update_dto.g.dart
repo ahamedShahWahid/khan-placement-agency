95600236ed4080cf027bac6d2f777caac7bc989e
Ahamed Shah <ahamadshah@LLPLHOLP571.local>
2026-05-27 18:36:24 +0530

    feat(app): ProfileUpdateDto request body

---
+++ app/lib/data/me/profile_update_dto.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile_update_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Map<String, dynamic> _$ProfileUpdateDtoToJson(ProfileUpdateDto instance) =>
+    <String, dynamic>{
+      'full_name': instance.fullName,
+      'locations': instance.locations,
+      'notice_period_days': instance.noticePeriodDays,
+      'current_ctc': instance.currentCtc,
+      'expected_ctc': instance.expectedCtc,
+      'years_experience': instance.yearsExperience,
+    };
